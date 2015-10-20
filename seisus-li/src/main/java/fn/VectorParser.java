bece48a4922c5934eae38147ffcde14481d4201a
alexrook <rook.alex@gmail.com>
2015-10-20 13:48:45 +0300

    добавил проверку match линий в x,y,z regex '4 числа разделенные пустыми пространствами'

--- seisus-li/src/main/java/fn/VectorParser.java
+++ seisus-li/src/main/java/fn/VectorParser.java
@@ -13,8 +13,8 @@ import java.text.ParseException;
  */
 public class VectorParser {
 
-
     public enum VectorType {
+
         X, Y, Z
     }
 
@@ -29,38 +29,41 @@ public class VectorParser {
         W w = W.getInstace();
 
         NumberFormat numberForm = NumberFormat.getInstance();
-        
+
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
             String line = reader.readLine();
             int i = 1;
             while (line != null) {
-                String[] fields = line.split("\t");
-                if (fields.length != 4) {
-                    throw new IOException("File: " + file.getName() + ", line: " + i + ", count of fields<>4");
-                }
+                if (checkLine(line)) {
+                    String[] fields = line.split("\\s");
+                    try {
+                        Double frecuency = numberForm.parse(fields[0]).doubleValue();
+                        switch (vt) {
 
-                try {
-                    Double frecuency = numberForm.parse(fields[0]).doubleValue();
-                    switch (vt) {
-
-                        case X:
-                            w.putX(frecuency, numberForm.parse(fields[2]).doubleValue()); //во всех файлах читаем 3 столбец
-                            break;
-                        case Y:
-                            w.putY(frecuency, numberForm.parse(fields[2]).doubleValue());
-                            break;
-                        case Z:
-                            w.putZ(frecuency, numberForm.parse(fields[2]).doubleValue());
-                            break;
+                            case X:
+                                w.putX(frecuency, numberForm.parse(fields[2]).doubleValue()); //во всех файлах читаем 3 столбец
+                                break;
+                            case Y:
+                                w.putY(frecuency, numberForm.parse(fields[2]).doubleValue());
+                                break;
+                            case Z:
+                                w.putZ(frecuency, numberForm.parse(fields[2]).doubleValue());
+                                break;
+                        }
+                    } catch (ParseException e) {
+                        throw new IOException("File: " + file.getName() + ", line: " + i + ", one of fields do not contain number");
                     }
-                } catch (ParseException e) {
-                    throw new IOException("File: " + file.getName() + ", line: " + i + ", one of fields do not contain number");
                 }
                 i++;
                 line = reader.readLine();
             }
+
         }
 
+    }
+
+    private boolean checkLine(String line) {
+        return line.matches("(([0-9,\\.]*)\\s){3}([0-9,\\.]*)");
     }
 
 }
